2a3787aed69192528ea9d1bef19e9ead78e1c1cf
RuiTong-716 <73323777+RuiTong-716@users.noreply.github.com>
2020-11-21 16:20:48 +0000

    merging two query

--- create_auction_result.php
+++ create_auction_result.php
@@ -41,24 +41,16 @@ else {
   $minIncrement = validate_minIncrement($conn, $_POST['minIncrement']);
 }
 
-// Convert the category name entered by the user into category ID that needs to be inserted
-$query1 = "SELECT categoryID FROM Categories WHERE categoryName = '$category'";
-$result1 = mysqli_query($conn, $query1);
-$row1 = mysqli_fetch_row($result1);
-$categoryID = $row1[0];
-if (!$result1) {
-  echo '<h2 class="my-3">Creation unsuccessful :(</h2><p>There was an error interacting with the database.</p><p>Click <a href="create_auction.php">here</a> to return to the auction creation page and try again.</p>';
-  mysqli_close($conn);
-  die();
-} 
+
 
 // Inserting required fields
 $sellerID = $_SESSION['userID'];
-$query2="INSERT INTO Auctions (title, descript, createDate, startDate, endDate, startPrice, reservePrice, minIncrement, sellerID, categoryID) 
-         VALUES ('$title', '$descript', '$createDate', '$startDate', '$endDate', '$startingPrice', '$reservePrice', '$minIncrement', '$sellerID', '$categoryID')";
-$result2 = mysqli_query($conn, $query2);
+$query1="INSERT INTO Auctions (title, descript, createDate, startDate, endDate, startPrice, reservePrice, minIncrement, sellerID, categoryID) 
+         VALUES ('$title', '$descript', '$createDate', '$startDate', '$endDate', '$startingPrice', '$reservePrice', '$minIncrement', '$sellerID',
+          (SELECT categoryID FROM Categories WHERE categoryName = '$category'))";
+$result1 = mysqli_query($conn, $query1);
 
-if ($result2) {
+if ($result1) {
   $auctionID = mysqli_insert_id($conn);
   echo "<h2 class='my-3'>Creation successful!</h2><p>Click <a href='listing.php?auctionID=".$auctionID."'>here</a> to view your listing, or wait 10 seconds to be redirected to the homepage.</p>";
   header("refresh:10; url=index.php");
